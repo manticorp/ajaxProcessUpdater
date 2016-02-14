79454edb5b68983d017886b5d8b441d59cfe98b1
Harry Mustoe-Playfair <manticorp1234@gmail.com>
2016-02-14 00:00:29 +0000

    Fixed tests to work with travis

--- tests/ProgressUpdaterTest.php
+++ tests/ProgressUpdaterTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__."./../Manticorp/ProgressUpdater.php";
-
 /**
  * @coversDefaultClass \Manticorp\ProgressUpdater
  */
--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,5 @@
 <?php
 
 require_once __DIR__."./../Manticorp/ProgressUpdater.php";
+require_once __DIR__."./../vendor/autoload.php";
 
